5320abc739f5fe46d2e300870f9286ee6b8feee6
Jim Stichnoth <stichnot@gmail.com>
2014-01-05 06:55:54 -0800

    Add license stuff.
    The LICENSE file is copied from the top-level Native Client source
    directory with the date changed to 2014.  The licensing comment in
    each source file is copied from Native Client files with the date
    changed to 2014.

--- IceInst.h
+++ IceInst.h
@@ -1,3 +1,8 @@
+/* Copyright 2014 The Native Client Authors. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can
+ * be found in the LICENSE file.
+ */
+
 // -*- Mode: c++ -*-
 #ifndef _IceInst_h
 #define _IceInst_h
