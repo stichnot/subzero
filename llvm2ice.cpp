719b5fa6f606e425fee12e9c52fb1507c9cd2ff6
Eli Bendersky <eliben@gmail.com>
2014-01-08 06:50:07 -0800

    Adding the basic ParseIRFile skeleton code to llvm2ice - parse and dump a module
    Also adding the ir_samples/ directory where (for now) some LLVM IR samples will
    live, along with the C files that clang compiles to them. At a later stage we
    may want a more elaborate generation scheme where we use the PNaCl toolchain to
    freeze and then thaw the module (so we see the stable PNaCl ABI-version of the
    IR).

--- llvm2ice.cpp
+++ llvm2ice.cpp
@@ -3,13 +3,31 @@
  * be found in the LICENSE file.
  */
 
+#include "llvm/IR/DataLayout.h"
+#include "llvm/IR/Instruction.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IRReader/IRReader.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/SourceMgr.h"
 
 using namespace llvm;
 
+int main(int argc, char **argv) {
+  if (argc < 2) {
+    errs() << "Usage: " << argv[0] << " <IR file>\n";
+    return 1;
+  }
 
-int main(int argc, char** argv) {
-  errs() << "x\n";
+  // Parse the input LLVM IR file into a module.
+  SMDiagnostic Err;
+  Module *Mod = ParseIRFile(argv[1], Err, getGlobalContext());
+  if (!Mod) {
+    Err.print(argv[0], errs());
+    return 1;
+  }
+
+  Mod->dump();
   return 0;
 }
-
