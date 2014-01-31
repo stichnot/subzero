7d66daf7fcd268894f3be4573d1a9940798749c3
Eli Bendersky <eliben@gmail.com>
2014-01-31 15:04:04 -0800

    Add the build-pnacl-ir.py script.
    This script uses tools from a NaCl SDK installation to compile C code to
    valid PNaCl bitcode in textual format.
    
    Note: this is *not* "final, translateable PNaCl bitcode", because that also
    implies the PNaCl .bc binary format which is incompatible with LLVM. The goal
    is to produce LLVM IR that constitutes valid PNaCl bitcode semantically.

---
+++ ir_samples/build-pnacl-ir.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python2
+
+import argparse
+import os, sys
+from subprocess import Popen, PIPE
+import tempfile
+
+def shellcmd(command, echo=True):
+    if echo: print '[cmd]', command
+    sb = Popen(command, stdout=PIPE, shell=True)
+    stdout_result = sb.communicate()[0]
+    if echo: sys.stdout.write(stdout_result)
+    return stdout_result
+
+if __name__ == '__main__':
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('cfile', nargs='+', type=str,
+        help='C file(s) to convert')
+    argparser.add_argument('--nacl_sdk_root', nargs='?', type=str,
+        help='Path to NACL_SDK_ROOT')
+    args = argparser.parse_args()
+
+    nacl_sdk_root = os.environ.get('NACL_SDK_ROOT', None)
+    if args.nacl_sdk_root:
+        nacl_sdk_root = os.path.expanduser(args.nacl_sdk_root)
+
+    if not nacl_sdk_root or not os.path.exists(nacl_sdk_root):
+        print '''\
+Please set the NACL_SDK_ROOT environment variable or pass the path through
+--nacl_sdk_root to point to a valid Native Client SDK installation.'''
+        sys.exit(1)
+
+    includes_path = os.path.join(nacl_sdk_root, 'include')
+    toolchain_path = os.path.join(nacl_sdk_root, 'toolchain', 'linux_pnacl')
+    clang_path = os.path.join(toolchain_path, 'bin64', 'pnacl-clang')
+    opt_path = os.path.join(toolchain_path, 'host_x86_64', 'bin', 'opt')
+
+    tempdir = tempfile.mkdtemp()
+
+    for cname in args.cfile:
+        basename = os.path.splitext(cname)[0]
+        llname = os.path.join(tempdir, basename + '.ll')
+        pnaclname = basename + '.pnacl.ll'
+
+        shellcmd(clang_path + ' -I{0} -c {1} -o {2}'.format(
+            includes_path, cname, llname))
+        shellcmd(opt_path +
+            ' -O2 -pnacl-abi-simplify-preopt -pnacl-abi-simplify-postopt'
+            ' -verify-pnaclabi-module -verify-pnaclabi-functions'
+            ' -pnaclabi-allow-debug-metadata -disable-simplify-libcalls'
+            ' {0} -S -o {1}'.format(llname, pnaclname))
+
+
